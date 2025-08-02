1385cb0924091a0f7fe5deee61fda695dd44140b
i-Abdulhaseeb <abdulhaseeb2894@gmail.com>
2025-08-02 22:23:17 +0500

    Initial commit

---
+++ lib/model/expense.dart
@@ -0,0 +1,19 @@
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+
+enum Category { work, food, leisure, travel }
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+  final String title;
+  final double amount;
+  final String id;
+  final DateTime date;
+  final Category category;
+}
